165b3dba60ecada3c1dc25bec90052e51755869d
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-03-28 14:35:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function htmlEncode (value){
+function htmlEncode(value) {
   return $('<div/>').text(value).html();
 }
 
 $(function() {
   $("#generate").click(function() {
-    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=160x160&chld=L|0");
+    var qrCodeURL = "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=540x540&chld=L|0";
+    $(".qr-code").attr("src", qrCodeURL);
+
+    // Set the href for the download link and make it visible
+    $("#download").attr("href", qrCodeURL).show();
   });
 });
