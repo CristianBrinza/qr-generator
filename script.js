039d819374fae0824bac9ad08735188712c18842
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-04-16 09:07:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function htmlEncode(value) {
 
 $(function() {
   $("#generate").click(function() {
-    var qrCodeURL = "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=540x540&chld=L|0";
+    var qrCodeURL = "https://quickchart.io/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=540x540&chld=L|0";
     $(".qr-code").attr("src", qrCodeURL);
 
     // Set the href for the download link and make it visible
     $("#download").attr("href", qrCodeURL).show();
   });
 });
+
