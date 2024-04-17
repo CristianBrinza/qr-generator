289ac5c9b4d2fe62ef28ccff13aa934901d59eeb
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-04-17 12:29:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 function htmlEncode(value) {
-  return $('<div/>').text(value).html();
+  return $("<div/>").text(value).html();
 }
 
 function downloadImage(url, filename) {
   fetch(url)
-    .then(response => response.blob())
-    .then(blob => {
+    .then((response) => response.blob())
+    .then((blob) => {
       // Create a URL for the blob object
       let blobUrl = window.URL.createObjectURL(blob);
 
       // Set the download link href to the blob URL and simulate a click to download
-      let downloadLink = document.getElementById('download');
+      let downloadLink = document.getElementById("download");
       downloadLink.href = blobUrl;
       downloadLink.download = filename;
-      downloadLink.style.display = 'block';  // Ensure the download link is visible
+      downloadLink.style.display = "block"; // Ensure the download link is visible
     })
-    .catch(error => console.error('Error downloading the image:', error));
+    .catch((error) => console.error("Error downloading the image:", error));
 }
 
-$(function() {
-  $("#generate").click(function() {
-    var content = $("#content").val();
-    var qrCodeURL = "https://quickchart.io/chart?cht=qr&chl=" + htmlEncode(content) + "&chs=540x540&chld=L|0";
-    $(".qr-code").attr("src", qrCodeURL);
+$(function () {
+  $("#generate").click(function () {
+    var content = $("#content").val().trim(); // Use trim to remove spaces from start and end
+    if (content !== "") {
+      var qrCodeURL =
+        "https://quickchart.io/chart?cht=qr&chl=" +
+        htmlEncode(content) +
+        "&chs=540x540&chld=L|0";
+      $(".qr-code").attr("src", qrCodeURL);
 
-    // Generate the downloadable link
-    downloadImage(qrCodeURL, 'QRCode.png');
+      // Generate the downloadable link
+      downloadImage(qrCodeURL, "QRCode.png");
+    } else {
+      alert("Error: empty content");
+    }
   });
 });
